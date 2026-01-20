279f6a940206a9fc70ff986061adc7e914d5768e
tuedavit <tuedavit7@gmail.com>
2026-01-20 22:01:06 +0700

    Update getChatId.js

--- getChatId.js
+++ getChatId.js
@@ -1,1 +1,14 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "7986795195:AAG_vTa2BZUdZXVRNuqSvkaaaCf-t5beMvE";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+console.log("🤖 Bot đang chạy...");
+console.log("👉 Mở Telegram, vào bot và nhắn: hi");
+
+bot.on("message", (msg) => {
+  console.log("✅ CHAT_ID =", msg.chat.id);
+  console.log("💬 MESSAGE =", msg.text);
+});
 
